d0308d1e3a26f03b525147484373d685a58a69f2
whonix2904 <113676978+whonix2904@users.noreply.github.com>
2023-11-24 21:51:35 +0530

    gsap :(

--- main.js
+++ main.js
@@ -1,33 +1,45 @@
 gsap.registerPlugin(ScrollTrigger);
 
-// var tl = gsap.timeline();
+let servicesList = document.querySelectorAll('.services');
 
-// tl.from ('.logo-section, .list-item,  .navbar-btn', {
-//     duration: 0.6,
-//     delay:0.5,
-//     translateX : "-10px",
-//     ease: "power2.out",
-//     stagger: 0.05,
-//     opacity:0,
-// });
+for (let i = 0; i < servicesList.length; i++) {
+    gsap.from(".servicesList[i]", {
+        y: '100',
+        duration: 2.0,
+        // opacity:0,
+        // stagger: 0.3,
+        scrollTrigger: {
+            trigger: ".services",
+            start: "top center",
+            end: "bottom center",
+            markers: true,
+        },
+    });
+};
 
-gsap.from ('.detail', {
-    duration: 2.8,
-    x: '-1000',
-    // ease: "power2.out",
-    // stagger: 0.2,
-    ScrollTrigger: {
-        trigger:'.detail',
-        markers:true,
-        scroller:"body",
-        // start:"top 70%",
-        // end:"bottom 90%"
-    }
+gsap.from ('.logo-section, .list-item,  .navbar-btn', {
+    duration: 0.6,
+    delay: 0.5,
+    translateX : "-10px",
+    ease: "power2.out",
+    stagger: 0.1,
+    opacity:0,
 });
 
-// tl.from ('.project-content', {
-//     duration: 2.8,
-//     x: '100',
-//     ease: "power2.out",
-//     stagger: 0.2,
-// });
+gsap.from ('.hero-content', {
+    duration: 1,
+    x: '100',
+    ease: "power2.out",
+    opacity:0,
+    stagger: 0,
+    delay:1.0,
+});
+
+gsap.from ('.hero-image-container', {
+    duration: 1,
+    x: '-100',
+    ease: "power2.out",
+    opacity:0,
+    stagger: 0,
+    delay:1.0,
+});
